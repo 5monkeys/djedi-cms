53b4e946d8975f420db54a0810f1cbf4cbf83a0d
Jonas Lundberg <jonas@5monkeys.se>
2013-09-19 15:54:36 +0200

    Setup include package data

--- setup.py
+++ setup.py
@@ -36,6 +36,8 @@ setup(
     keywords=['cms', 'django', 'edit', 'gettext', 'content', 'management', 'template', 'plugins', 'markdown'],
     license='BSD',
     packages=find_packages(exclude='tests'),
+    include_package_data=True,
+    zip_safe=False,
     classifiers=[
         'Environment :: Web Environment',
         'Natural Language :: English',
