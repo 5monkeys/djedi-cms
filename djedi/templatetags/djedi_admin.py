232bffd6e41b87b05d9e0c65401ace3163bfc799
Jonas Lundberg <jonas@5monkeys.se>
2013-09-30 15:16:11 +0200

    Clear content-io pipeline after rendering admin panel

--- djedi/templatetags/djedi_admin.py
+++ djedi/templatetags/djedi_admin.py
@@ -12,9 +12,14 @@ logger = logging.getLogger(__name__)
 @register.simple_tag(takes_context=True)
 def djedi_admin(context):
     output = u''
+
     if has_permission(context.get('user')):
         defaults = dict((node.uri.clone(version=None), node.initial) for node in pipeline.history.list('get'))
         output = render_to_string('djedi/cms/embed.html', {
             'json_nodes': json.dumps(defaults).replace('</', '\\x3C/'),
         })
+
+    # Clear pipeline
+    pipeline.clear()
+
     return output
