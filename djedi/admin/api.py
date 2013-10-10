ce56d1f63d3ac51b1ab526970921cfef9288b1ba
Hannes Ljungberg <hannes@5monkeys.se>
2013-10-10 11:57:17 +0200

    Support Django 1.5< custom user models where username field has been changed.

--- djedi/admin/api.py
+++ djedi/admin/api.py
@@ -96,7 +96,7 @@ class NodeApi(JSONResponseMixin, APIView):
         """
         uri = self.decode_uri(uri)
         data, meta = self.get_post_data(request)
-        meta['author'] = request.user.username
+        meta['author'] = auth.get_username(request.user)
         node = cio.set(uri, data, publish=False, **meta)
         return self.render_to_json(node)
 
