b0694c652c6cd2f491f3425c637a83cc4bcd1aae
Joakim Saario <saario.joakim@gmail.com>
2016-08-26 19:43:39 +0200

    Fix flake8 errors

--- djedi/tests/__init__.py
+++ djedi/tests/__init__.py
@@ -1,5 +1,5 @@
-from .test_admin import *
-from .test_cache import *
-from .test_rest import *
-from .test_settings import *
-from .test_templatetags import *
+from .test_admin import *  # noqa
+from .test_cache import *  # noqa
+from .test_rest import *  # noqa
+from .test_settings import *  # noqa
+from .test_templatetags import *  # noqa
