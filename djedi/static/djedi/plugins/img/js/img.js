6fb2ab415e9f89c87e7e65e6a71275b9e980460f
Leolainen <leolainen2@gmail.com>
2018-11-28 13:52:59 +0100

    Prevent bug with outlines when rendering images by letting the image finish loading before measuring its dimensions

--- djedi/static/djedi/plugins/img/js/img.js
+++ djedi/static/djedi/plugins/img/js/img.js
@@ -89,7 +89,11 @@
         display: 'inline-block'
       });
       this.previewContainer.append(image);
-      return this.$img.trigger('crop:preview', this.previewContainer);
+      return image.onload = (function(_this) {
+        return function() {
+          return _this.$img.trigger('crop:preview', _this.previewContainer);
+        };
+      })(this);
     };
 
     CropTool.prototype.setPreviewAttributes = function(attrs) {
