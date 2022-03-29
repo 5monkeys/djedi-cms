9b4a4ed1fd2d55208fad96ad1a072ef045cc2a53
Mika Ivarsson <13712961+Mojken@users.noreply.github.com>
2022-03-29 08:55:05 +0200

    Try generating only one coverage files
    Instead of for each matrix step

--- djedi/compat.py
+++ djedi/compat.py
@@ -7,16 +7,11 @@ from django.shortcuts import render
 from django.template.loader import render_to_string
 from django.template.response import TemplateResponse as BaseTemplateResponse
 
-if django.VERSION < (1, 6):
-    from django.conf.urls.defaults import include, url
-else:
-    from django.conf.urls import include, url
+
+from django.conf.urls import include, url
 
 
-if django.VERSION < (2, 0):
-    from django.core.urlresolvers import NoReverseMatch, reverse
-else:
-    from django.urls import NoReverseMatch, reverse
+from django.urls import reverse, NoReverseMatch
 
 
 def patterns(*urls):
@@ -32,27 +27,22 @@ def patterns(*urls):
 
 
 if django.VERSION >= (1, 9):
-    if django.VERSION < (2, 0):
-        from django.template.library import parse_bits
-    else:
 
-        def parse_bits(
-            parser, bits, params, varargs, varkw, defaults, takes_context, name
-        ):
-            from django.template import library
+    def parse_bits(parser, bits, params, varargs, varkw, defaults, takes_context, name):
+        from django.template import library
 
-            return library.parse_bits(
-                parser=parser,
-                bits=bits,
-                params=params,
-                varargs=varargs,
-                varkw=varkw,
-                defaults=defaults,
-                kwonly=(),
-                kwonly_defaults=(),
-                takes_context=takes_context,
-                name=name,
-            )
+        return library.parse_bits(
+            parser=parser,
+            bits=bits,
+            params=params,
+            varargs=varargs,
+            varkw=varkw,
+            defaults=defaults,
+            kwonly=(),
+            kwonly_defaults=(),
+            takes_context=takes_context,
+            name=name,
+        )
 
     def generic_tag_compiler(
         parser, token, params, varargs, varkw, defaults, name, takes_context, node_class
@@ -70,10 +60,6 @@ if django.VERSION >= (1, 9):
         )
         return node_class(takes_context, args, kwargs)
 
-else:
-    from django.template.base import generic_tag_compiler  # noqa
-    from django.template.base import parse_bits
-
 
 if django.VERSION >= (1, 8):
     # Always use the Django template engine on Django 1.8.
@@ -83,10 +69,7 @@ if django.VERSION >= (1, 8):
     class TemplateResponse(BaseTemplateResponse):
         def __init__(self, *args, **kwargs):
             kwargs["using"] = "django"
-            super().__init__(*args, **kwargs)
-
-else:
-    TemplateResponse = BaseTemplateResponse
+            super(TemplateResponse, self).__init__(*args, **kwargs)
 
 
 if django.VERSION < (1, 9):
