bc98fb8af9c23d9c99df43e3f83cfe457116dd2c
Mojken <13712961+Mojken@users.noreply.github.com>
2022-03-31 14:59:45 +0200

    Remove artifact from faulty merge

--- djedi/admin/api.py
+++ djedi/admin/api.py
@@ -15,7 +15,6 @@ from cio.plugins.exceptions import UnknownPlugin
 from cio.utils.uri import URI
 
 from .. import auth
-from ..compat import TemplateResponse
 from ..plugins.base import DjediPlugin
 from .exceptions import InvalidNodeData
 from .mixins import DjediContextMixin, JSONResponseMixin
